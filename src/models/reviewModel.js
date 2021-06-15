f1e979f28e990ec98d16ae1001a35bcec609f5c3
swati317 <40908620+swati317@users.noreply.github.com>
2021-06-15 09:44:28 +0530

    Create reviewModel.js

---
+++ src/models/reviewModel.js
@@ -0,0 +1,11 @@
+export class Review {
+    constructor(obj) {
+        this.review = obj.review;
+        this.author = obj.author;
+        this.review_source = obj.review_source;
+        this.rating = obj.rating;
+        this.title = obj.title;
+        this.product_name = "Amazon Alexa";
+        this.reviewed_date = new Date().toISOString();
+    }
+}
